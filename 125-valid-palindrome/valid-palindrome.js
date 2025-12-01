743c25b87981a44510793806ad63d24d4472333c
ph-nathan <99804772+ph-nathan@users.noreply.github.com>
2025-12-01 23:16:37 +0700

    Time: 3 ms (85.96%) | Memory: 57.4 MB (76.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,12 +2,7 @@
  * @param {string} s
  * @return {boolean}
  */
-function isAlphaNumericChar (char) {
-    const unicodeVal = char.charCodeAt(0);
-    return (unicodeVal <= 57 && unicodeVal >= 48) 
-    || (unicodeVal <= 90 && unicodeVal >= 65) 
-    || (unicodeVal <= 122 && unicodeVal >= 97);
-} 
+
 var isPalindrome = function(s) {
     s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
     let l = 0;
@@ -22,3 +17,27 @@ var isPalindrome = function(s) {
     }
     return true;
 }
+
+/** 
+function isAlphaNumericChar (char) {
+    const unicodeVal = char.charCodeAt(0);
+    return (unicodeVal <= 57 && unicodeVal >= 48) 
+    || (unicodeVal <= 90 && unicodeVal >= 65) 
+    || (unicodeVal <= 122 && unicodeVal >= 97);
+} 
+var isPalindrome = function(s) {
+    const charArr = [...s];
+    const filtered = charArr.filter(x => isAlphaNumericChar(x)).map(x => x.toLowerCase());
+    let l = 0;
+    let r = filtered.length - 1;
+
+    while (l <= r) {
+        if (filtered[l] !== filtered[r]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
+*/
