8f4792a3b635ccbe936fe42c8286ddd0fa92e4fd
ph-nathan <99804772+ph-nathan@users.noreply.github.com>
2026-03-24 16:30:34 +0800

    Time: 26 ms (22.58%) | Memory: 59 MB (33.77%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,9 +4,21 @@
  * @return {boolean}
  */
 
+
 // Intuition: check if the # of occurences of character of the two words match
 
+// O(NlogN), sorting then compare 2 strings
 function isAnagram(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const sortedS = s.split('').sort().join('');
+    const sortedT = t.split('').sort().join('');
+    return sortedS === sortedT;
+}
+// O(N) time, O(1) space
+function isAnagramv2(s, t) {
     if (s.length !== t.length) {
         return false;
     }
@@ -25,3 +37,11 @@ function isAnagram(s, t) {
 
     return histogram.every(val => val === 0);
 };
+// Follow-up
+
+/*
+    "If the input space is no longer limited to 26 lowercase English letters, 
+    the fixed array approach not optimal because the character set is too massive and sparse. 
+    In that case, I would use The Hash Map 
+    A Hash Map perfectly handles a massive, unpredictable character set while keeping the time complexity at O(N)"
+*/
