3a9db9ec712da1175179a1c4d58adc02873cd9b0
ph-nathan <99804772+ph-nathan@users.noreply.github.com>
2026-03-24 17:09:47 +0800

    Time: 1 ms (87.04%) | Memory: 54.8 MB (56.29%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -17,8 +17,34 @@ function twoSumv0(nums, target) {
     return [-1, -1]
 }
 
-// use Map to store index of other element 
+// Sort -> Two pointers
+// Time: O(n log n) (due to sorting)
+// Space: O(n) (to store the original indices)
+function twoSumv1(nums, target) {
+    const paired = nums.map((num,idx) => [num, idx]).sort((a, b) => a[0] - b[0]);
+    
+    let l = 0;
+    let r = nums.length - 1;
+    while (l < r) {
+        const left = paired[l][0];
+        const right = paired[r][0];
+        const sum = left + right;
 
+        if (sum === target) {
+            return [paired[l][1], paired[r][1]];
+        }
+        if (sum < target) {
+            l++; // Need a bigger number
+        } else {
+            r--; // Need a smaller number
+        }
+    }
+    return [-1, -1]
+    
+}
+
+// Optimal: use Map to store index of other element 
+// O(n) space, time
 function twoSum(nums, target) {
     const numsMap = {};
     for (let i = 0; i < nums.length; i++) {
