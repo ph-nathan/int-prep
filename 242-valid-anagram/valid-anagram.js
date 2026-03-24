23f2257f0392548a4961bc3db2c50de27017aa5f
ph-nathan <99804772+ph-nathan@users.noreply.github.com>
2026-03-24 16:35:55 +0800

    Time: 15 ms (50.15%) | Memory: 55.7 MB (75.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -7,8 +7,8 @@
 
 // Intuition: check if the # of occurences of character of the two words match
 
-// O(NlogN), sorting then compare 2 strings
-function isAnagram(s, t) {
+// Naive: O(NlogN), sorting then compare 2 strings
+function isAnagramv0(s, t) {
     if (s.length !== t.length) {
         return false;
     }
@@ -17,12 +17,34 @@ function isAnagram(s, t) {
     const sortedT = t.split('').sort().join('');
     return sortedS === sortedT;
 }
+
+// Frequency related to Map -> O(N) time and space
+function isAnagram(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    const freqMap = new Map();
+    for (let i = 0 ; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+
+        freqMap.set(charS, (freqMap.get(charS) ?? 0) + 1);
+        freqMap.set(charT, (freqMap.get(charT) ?? 0) - 1);
+
+    }
+
+    // how to convert map values to array
+    // .values() return iterable object, loop thru using for of
+    return [...freqMap.values()].every(val => val === 0);
+}
+
 // O(N) time, O(1) space
 function isAnagramv2(s, t) {
     if (s.length !== t.length) {
         return false;
     }
 
+    // 26 lowercase characters
     const histogram = new Array(26).fill(0);
 
     const baseVal = 'a'.charCodeAt(0);
