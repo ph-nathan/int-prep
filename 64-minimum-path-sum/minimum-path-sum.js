4754b9ae421abb1ba2a6986f7301455dfb063e9e
ph-nathan <99804772+ph-nathan@users.noreply.github.com>
2026-03-24 15:17:26 +0800

    Time: 9 ms (24.75%) | Memory: 58.9 MB (16.13%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.js
+++ 64-minimum-path-sum/minimum-path-sum.js
@@ -1,8 +1,9 @@
-/**
- * @param {number[][]} grid
- * @return {number}
- */
-function minPathSum(grid) {
+/*
+    Approach 1: Top-down DP:
+    Start at the end (minSum(bottom_right) then solve for smaller sub-problems)
+*/
+ 
+function minPathSumv0(grid) {
     const rows = grid.length;
     const cols = grid[0].length;
 
@@ -22,4 +23,34 @@ function minPathSum(grid) {
         return memo[r][c];
     }
     return minSum(rows - 1, cols - 1);
+}
+
+/*
+    Approach 2: Bottom-up DP:
+    Start at the top-left and loop thru table to find min sum at bottom right
+*/
+
+function minPathSum(grid) {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const dp = new Array(rows).fill(null).map(x => new Array(cols).fill(0));
+
+    dp[0][0] = grid[0][0];
+
+    // all positions in first row is only reachable from direct left
+    for (let c = 1; c < cols; c++) {
+        dp[0][c] = grid[0][c] + dp[0][c - 1];
+    }
+
+    // all positions in first col is only reachable from direct top 
+    for (let r = 1; r < rows; r++) {
+        dp[r][0] = grid[r][0] + dp[r - 1][0];
+    }
+    for (let r = 1; r < rows; r++) {
+        for (let c = 1; c < cols; c++) {
+            dp[r][c] = Math.min(dp[r - 1][c], dp[r][c - 1]) + grid[r][c];
+        }
+    }
+
+    return dp[rows - 1][cols - 1];
 }
