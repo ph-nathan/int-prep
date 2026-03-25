a7571f0647781a0a03779a04260202f1ee4b57b3
ph-nathan <99804772+ph-nathan@users.noreply.github.com>
2026-03-25 10:52:27 +0800

    Time: 0 ms (100.00%) | Memory: 54.2 MB (58.60%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -2,7 +2,12 @@
  * @param {string[]} strs
  * @return {string}
  */
-function longestCommonPrefix(strs) {
+// Learning Points:
+// - slice / splice
+// 
+// Naive:
+// Compare strings one at a time, keep a common string 
+function longestCommonPrefixv0(strs) {
     let common = strs[0];
 
     for (const str of strs) {
@@ -17,3 +22,17 @@ function longestCommonPrefix(strs) {
 
     return common;
 };
+
+function longestCommonPrefix(strs) {
+    strs.sort();
+
+    const first = strs[0];
+    const last = strs[strs.length - 1];
+    
+    for (let i = 0; i < first.length; i++) {
+        if (first[i] !== last[i]) {
+            return first.slice(0, i);
+        }
+    }
+    return first;  
+}
