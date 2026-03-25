c564dd22713df95244893dfd17f2f37128600af9
ph-nathan <99804772+ph-nathan@users.noreply.github.com>
2026-03-25 16:05:32 +0800

    Time: 126 ms (5.03%) | Memory: 69.8 MB (12.50%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -5,7 +5,7 @@
 
 // Naive: anagrams have the same form sorted
 // -> use it as key to group anagram together
-function groupAnagrams(strs) {
+function groupAnagramsv1(strs) {
     const groups = {};
     
     for (const str of strs) {
@@ -22,3 +22,30 @@ function groupAnagrams(strs) {
 };
 
 // eficient instead of using sorted, can use histogram string as key
+
+
+
+function groupAnagrams(strs) {
+    const groups = {};
+
+    for (const str of strs) {
+        const histogram = new Array(26).fill(0);
+
+        for (let i = 0; i < str.length; i++) {
+            histogram[str.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        }
+
+        // arr numbers -> string
+        const key = histogram.reduce((acc, init) => (acc + ',' + init),'');
+
+        console.log(key);
+
+        if (!groups[key]) {
+            groups[key] = [];
+        }
+        groups[key].push(str);
+    }
+
+    return Object.values(groups);
+
+}
