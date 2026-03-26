41a072ff7d454ada8a05e5b6b1ffa52eb2ee2887
ph-nathan <99804772+ph-nathan@users.noreply.github.com>
2026-03-27 00:57:52 +0800

    Time: 3 ms (62.60%) | Memory: 57.4 MB (50.18%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -23,3 +23,22 @@ var majorityElement = function(nums) {
     return -1;
     
 };
+
+
+// optimal
+// booyer mayor algo
+
+var majorityElement = function(nums) {
+    let candidate;
+    let count = 0;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count+= num === candidate ? 1 : -1 
+    }
+
+    return candidate;
+
+}
